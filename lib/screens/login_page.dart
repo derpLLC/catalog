4338728f81e6fa2037bb20f56b13a996ca772b29
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-01 19:55:02 +0530

    lib: login: fix overflow by wraping the Column inside a ScrollView
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -7,56 +7,58 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset(
-              "assets/images/login.png",
-            fit: BoxFit.cover,
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Text(
-              "Welcome",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 24.0,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+                "assets/images/login.png",
+              fit: BoxFit.cover,
             ),
-          ),
-          SizedBox(
+            SizedBox(
               height: 20.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Enter Username",
-                    labelText: "Username",
-                  ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: "Enter Password",
-                    labelText: "Password",
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                ElevatedButton(
-                  child: Text('Login'),
-                  style: TextButton.styleFrom(),
-                  onPressed: () {
-                    print('Login Successful');
-                  },
-                ),
-              ],
             ),
-          ),
-        ],
+            Text(
+                "Welcome",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24.0,
+              ),
+            ),
+            SizedBox(
+                height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    child: Text('Login'),
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print('Login Successful');
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
