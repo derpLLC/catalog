6042825392fb3c55590cde41f61c4e0ef9a07d09
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-01 19:13:30 +0530

    lib: login: add spacing in between the text and at the end of the text
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -13,12 +13,18 @@ class LoginPage extends StatelessWidget {
               "assets/images/login.png",
             fit: BoxFit.cover,
           ),
+          SizedBox(
+            height: 20.0,
+          ),
           Text(
               "Welcome",
             style: TextStyle(
               fontWeight: FontWeight.bold,
               fontSize: 24.0,
             ),
+          ),
+          SizedBox(
+              height: 20.0,
           ),
         ],
       ),
