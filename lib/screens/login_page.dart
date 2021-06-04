d22e59617d4017194951403d0d70fb0cd88fe57d
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-04 19:44:38 +0530

    assests: images: update the login page image
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -20,7 +20,7 @@ class _LoginPageState extends State<LoginPage> {
         child: Column(
           children: [
             Image.asset(
-                "assets/images/login.png",
+                "assets/images/hey.png",
               fit: BoxFit.cover,
             ),
             SizedBox(
