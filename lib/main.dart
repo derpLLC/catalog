9ec812d2d45437810f6867877391dc50fb29a7a3
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-05 18:13:46 +0530

    lib: main: add some themeing changes to the app bar
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,13 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: ThemeData(
           fontFamily: GoogleFonts.lato().fontFamily,
-          primarySwatch: Colors.deepPurple
+          primarySwatch: Colors.deepPurple,
+        appBarTheme: AppBarTheme(
+          color: Colors.white,
+          elevation: 0.0,
+          iconTheme: IconThemeData(color: Colors.black),
+          textTheme: Theme.of(context).textTheme,
+        ),
       ),
       debugShowCheckedModeBanner: false,
       initialRoute: MyRoutes.homeRoute,
