f6b77e264aaac588723bf7fac17e374ba28adc6f
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-06 20:49:10 +0530

    lib: item_widget: Wrap the ListTile inside a Card widget
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -9,16 +9,18 @@ class ItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Image.network(item.image),
-      title: Text(item.name),
-      subtitle: Text(item.desc),
-      trailing: Text("\$${item.price}",
-      textScaleFactor: 1.5,
-      style: TextStyle(
-        color: Colors.deepPurple,
-        fontWeight: FontWeight.bold,
-      ),),
+    return Card(
+      child: ListTile(
+        leading: Image.network(item.image),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text("\$${item.price}",
+        textScaleFactor: 1.5,
+        style: TextStyle(
+          color: Colors.deepPurple,
+          fontWeight: FontWeight.bold,
+        ),),
+      ),
     );
   }
 }
