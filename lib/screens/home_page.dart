e63537c89e17cdeb8627f7c5a6ea68031639c99e
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-06 20:54:15 +0530

    lib: home_page: display a dummy list
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -8,6 +8,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Catalog App'),
@@ -16,10 +18,10 @@ class HomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView.builder(
-          itemCount: CatalogModel.items.length,
+          itemCount: dummyList.length,
             itemBuilder: (context, index){
             return ItemWidget(
-              item: CatalogModel.items[index],
+              item: dummyList[index],
             );
             }
         ),
