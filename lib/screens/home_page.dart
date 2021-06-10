97ba9935ddf64548210dc1f2366bb45080326551
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-10 20:33:17 +0530

    lib: home_page: add some padding to the Grid View builder
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -43,6 +43,8 @@ class _HomePageState extends State<HomePage> {
             ? GridView.builder(
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
                 ),
                 itemBuilder: (context, index) {
                   final item = CatalogModel.items[index];
