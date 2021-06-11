3c387b824033d020a5867fecf28f53ed5523418e
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-11 20:47:24 +0530

    lib: home_page: add Text name for the items
    *use text name from catalog.name
    
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -95,7 +95,17 @@ class CatalogItem extends StatelessWidget {
     return VxBox(
         child: Row(
       children: [
-        CatalogImage(image: catalog.image),
+        CatalogImage(
+            image: catalog.image
+        ),
+        Expanded(child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            catalog.name.text.lg.color(MyTheme.darkBluishColor).bold.make(),
+          ],
+        ),
+        )
       ],
     )).white.roundedLg.square(150).make().py16();
   }
