7a6dafa5b576818f5fca768181b7604a3cf4dafa
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-16 19:26:00 +0530

    lib: cart_page: add some basic stuff and styling
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screens/cart_page.dart
+++ lib/screens/cart_page.dart
@@ -1,9 +1,17 @@
+import 'package:catalog/widgets/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: MyTheme.creamColor,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        title: "Cart".text.make(),
+      ),
+    );
   }
 }
