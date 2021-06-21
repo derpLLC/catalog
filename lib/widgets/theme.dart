8f474f139969f9ad00a7c1884e90ef5d7c59d2b7
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-21 19:05:30 +0530

    lib: theme: copy some theme data to darkMode
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/widgets/theme.dart
+++ lib/widgets/theme.dart
@@ -12,8 +12,16 @@ class MyTheme {
           textTheme: Theme.of(context).textTheme,
         ),
       );
+
   static ThemeData darkTheme(BuildContext context) => ThemeData(
-        brightness: Brightness.dark,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+        primarySwatch: Colors.deepPurple,
+        appBarTheme: AppBarTheme(
+          color: Colors.white,
+          elevation: 0.0,
+          iconTheme: IconThemeData(color: Colors.black),
+          textTheme: Theme.of(context).textTheme,
+        ),
       );
 
   //Colors
