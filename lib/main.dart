03c086a75b3b3910933eae573c465d745b267011
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-21 19:19:31 +0530

    [DNM]lib: theme: set the theme to dark for now
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.dark,
       theme: MyTheme.lightTheme(context),
       darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
