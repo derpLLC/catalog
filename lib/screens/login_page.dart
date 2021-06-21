df5165e1592d7fb78479f125d8bddf7cd1c8da18
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-21 20:50:31 +0530

    lib: login: change themeing for login page
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:catalog/utils/routes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -29,7 +30,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.white,
+      color: context.canvasColor,
       child: SingleChildScrollView(
         child: Form(
           key: _formKey,
@@ -95,7 +96,7 @@ class _LoginPageState extends State<LoginPage> {
                     Material(
                       borderRadius:
                           BorderRadius.circular(changeButton ? 50 : 8.0),
-                      color: Colors.deepPurple,
+                      color: context.theme.buttonColor,
                       child: InkWell(
                         onTap: () => moveToHome(context),
                         child: AnimatedContainer(
