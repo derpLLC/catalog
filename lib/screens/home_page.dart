c44b37f1c862595bc64ae68f933d9391f1eea7e5
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-21 19:17:58 +0530

    lib: home_page: change the background color to canvasColor
    *use a deeper shade of gray
    
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -36,7 +36,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: MyTheme.creamColor,
+      backgroundColor: context.canvasColor, //Theme.of(context).canvasColor if not using VelocityX
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.pushNamed(context, MyRoutes.cartRoute);
--- lib/widgets/theme.dart
+++ lib/widgets/theme.dart
@@ -17,6 +17,8 @@ class MyTheme {
   static ThemeData darkTheme(BuildContext context) => ThemeData(
         fontFamily: GoogleFonts.poppins().fontFamily,
         primarySwatch: Colors.deepPurple,
+        cardColor: Colors.black,
+        canvasColor: darkCreamColor,
         appBarTheme: AppBarTheme(
           color: Colors.white,
           elevation: 0.0,
@@ -27,7 +29,7 @@ class MyTheme {
 
   //Colors
   static Color creamColor = Color(0xfff5f5f5);
-  static Color darkCreamColor = Vx.gray800;
+  static Color darkCreamColor = Vx.gray900;
   static Color darkBluishColor = Color(0xff403b58);
   static Color lightBluishColor = Vx.indigo500;
 }
