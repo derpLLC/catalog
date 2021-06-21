49f94b8460d0adab416e4faed3b6573ef0a7a8b8
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-21 19:12:24 +0530

    lib: theme: add some more constant shades
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/widgets/theme.dart
+++ lib/widgets/theme.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
@@ -26,5 +27,7 @@ class MyTheme {
 
   //Colors
   static Color creamColor = Color(0xfff5f5f5);
+  static Color darkCreamColor = Vx.gray800;
   static Color darkBluishColor = Color(0xff403b58);
+  static Color lightBluishColor = Vx.indigo500;
 }
