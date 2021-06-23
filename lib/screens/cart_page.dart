f6a9598e61a4146b150c086f6c3a752f12b72ac7
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-23 22:45:25 +0530

    lib: cart_page: set cart page background color as per canvas context
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screens/cart_page.dart
+++ lib/screens/cart_page.dart
@@ -1,4 +1,3 @@
-import 'package:catalog/widgets/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -6,7 +5,7 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: MyTheme.creamColor,
+      backgroundColor: context.canvasColor,
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: Colors.transparent,
