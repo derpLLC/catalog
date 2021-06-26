c239783e3a012f8f22e6d23f0a832c07a443484c
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-26 12:34:22 +0530

    lib: cart_page: take the items in the cart from CartModel
    * use card length
    
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screens/cart_page.dart
+++ lib/screens/cart_page.dart
@@ -1,3 +1,4 @@
+import 'package:catalog/models/cart.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -61,8 +62,9 @@ class _CartList extends StatefulWidget {
 class __CartListState extends State<_CartList> {
   @override
   Widget build(BuildContext context) {
+    final _cart = CartModel();
     return ListView.builder(
-      itemCount: 5,
+      itemCount: _cart.items?.length,
       itemBuilder: (context, index) => ListTile(
         leading: Icon(Icons.done),
         trailing: IconButton(
