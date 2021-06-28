0009e85f15f9be68df7bcde98f08941fecb20391
Ayush Dubey <ayushdubey70@gmail.com>
2021-06-28 21:17:51 +0530

    lib: refactor Statefull => Stateless
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/widgets/home_widgets/add_to_cart.dart
+++ lib/widgets/home_widgets/add_to_cart.dart
@@ -4,28 +4,23 @@ import 'package:catalog/models/cart.dart';
 import 'package:catalog/models/catalog.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-class AddToCart extends StatefulWidget {
+class AddToCart extends StatelessWidget {
   final Item catalog;
-  const AddToCart({Key key, this.catalog}) : super(key: key);
-
-  @override
-  _AddToCartState createState() => _AddToCartState();
-}
-
-class _AddToCartState extends State<AddToCart> {
-  final _cart = CartModel();
+  AddToCart({Key key, this.catalog}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    bool isInCart = _cart.items.contains(widget.catalog) ?? false;
+    final _cart = CartModel();
+
+    bool isInCart = _cart.items.contains(catalog) ?? false;
     return ElevatedButton(
       onPressed: () {
         if (!isInCart) {
           isInCart = isInCart.toggle();
           final _catalog = CatalogModel();
           _cart.catalog = _catalog;
-          _cart.add(widget.catalog);
-          setState(() {});
+          _cart.add(catalog);
+          // setState(() {});
         }
       },
       style: ButtonStyle(
