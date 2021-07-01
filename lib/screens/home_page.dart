246495ca0af29fbb97e6cccdcf5716a4601ffcb4
Ayush Dubey <ayushdubey70@gmail.com>
2021-07-01 21:36:13 +0530

    lib: home_page: remove unused import
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -8,7 +8,6 @@ import 'package:catalog/widgets/home_widgets/catalog_header.dart';
 import 'package:catalog/widgets/home_widgets/catalog_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:http/http.dart' as http;
 
