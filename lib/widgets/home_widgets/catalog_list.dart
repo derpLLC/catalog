461d4bab03fde5554892745c0deac11fb9aca68a
Ayush Dubey <ayushdubey70@gmail.com>
2021-07-02 21:07:10 +0530

    lib: catalog_list: extract hero widget into a local variable
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/widgets/home_widgets/catalog_list.dart
+++ lib/widgets/home_widgets/catalog_list.dart
@@ -56,33 +56,41 @@ class CatalogItem extends StatelessWidget {
         super(key: key);
   @override
   Widget build(BuildContext context) {
+    var children2 = [
+      Hero(
+        tag: Key(catalog.id.toString()),
+        child: CatalogImage(
+          image: catalog.image,
+        ),
+      ),
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            catalog.name.text.lg.color(context.accentColor).bold.make(),
+            catalog.desc.text.textStyle(context.captionStyle).make(),
+            10.heightBox,
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceBetween,
+              buttonPadding: EdgeInsets.zero,
+              children: [
+                "\$${catalog.price}".text.bold.xl.make(),
+                AddToCart(catalog: catalog)
+              ],
+            ).pOnly(right: 8.0)
+          ],
+        ).p(context.isMobile ? 0 : 16),
+      )
+    ];
     return VxBox(
-        child: Row(
-      children: [
-        Hero(
-          tag: catalog.image.toString(),
-          child: CatalogImage(image: catalog.image),
-        ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              catalog.name.text.lg.color(context.accentColor).bold.make(),
-              catalog.desc.text.textStyle(context.captionStyle).make(),
-              10.heightBox,
-              ButtonBar(
-                alignment: MainAxisAlignment.spaceBetween,
-                buttonPadding: EdgeInsets.zero,
-                children: [
-                  "\$${catalog.price}".text.bold.xl.make(),
-                  AddToCart(catalog: catalog),
-                ],
-              ).pOnly(right: 8.0),
-            ],
-          ),
-        ),
-      ],
-    )).color(context.cardColor).roundedLg.square(150).make().py16();
+      child: context.isMobile
+          ? Row(
+              children: children2,
+            )
+          : Column(
+              children: children2,
+            ),
+    ).color(context.cardColor).rounded.square(150).make().py16();
   }
 }
