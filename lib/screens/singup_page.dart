cc298410cc42e258526d1f758bb9b04355a6e7a1
Ayush Dubey <ayushdubey70@gmail.com>
2021-07-10 22:30:32 +0530

    lib: singup_page: fix render overflow
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/screens/singup_page.dart
+++ lib/screens/singup_page.dart
@@ -14,96 +14,98 @@ class SignUpPage extends StatelessWidget {
       body: SafeArea(
         child: Container(
           padding: Vx.m32,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              "Sign Up".text.xl5.bold.color(context.theme.accentColor).make(),
-              "Create your account".text.xl2.make(),
-              CupertinoFormSection(
-                header: "User Details".text.make(),
-                children: [
-                  CupertinoFormRow(
-                    child: CupertinoTextFormFieldRow(
-                      placeholder: "Enter Name",
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                "Sign Up".text.xl5.bold.color(context.theme.accentColor).make(),
+                "Create your account".text.xl2.make(),
+                CupertinoFormSection(
+                  header: "User Details".text.make(),
+                  children: [
+                    CupertinoFormRow(
+                      child: CupertinoTextFormFieldRow(
+                        placeholder: "Enter Name",
+                      ),
+                      prefix: "Name".text.make(),
                     ),
-                    prefix: "Name".text.make(),
-                  ),
-                  CupertinoFormRow(
-                    child: CupertinoTextFormFieldRow(
-                      placeholder: "Enter Phone",
+                    CupertinoFormRow(
+                      child: CupertinoTextFormFieldRow(
+                        placeholder: "Enter Phone",
+                      ),
+                      prefix: "Phone".text.make(),
                     ),
-                    prefix: "Phone".text.make(),
-                  ),
-                ],
-              ),
-              CupertinoFormSection(
-                header: "User".text.make(),
-                children: [
-                  CupertinoFormRow(
-                    child: CupertinoTextFormFieldRow(
-                      placeholder: "Enter Email",
+                  ],
+                ),
+                CupertinoFormSection(
+                  header: "User".text.make(),
+                  children: [
+                    CupertinoFormRow(
+                      child: CupertinoTextFormFieldRow(
+                        placeholder: "Enter Email",
+                      ),
+                      prefix: "Email".text.make(),
                     ),
-                    prefix: "Email".text.make(),
-                  ),
-                  CupertinoFormRow(
-                    child: CupertinoTextFormFieldRow(
-                      obscureText: true,
-                      placeholder: "Enter Password",
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          return "Password cannot be empty";
-                        } else
-                          return null;
-                      },
+                    CupertinoFormRow(
+                      child: CupertinoTextFormFieldRow(
+                        obscureText: true,
+                        placeholder: "Enter Password",
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return "Password cannot be empty";
+                          } else
+                            return null;
+                        },
+                      ),
+                      prefix: "Password".text.make(),
                     ),
-                    prefix: "Password".text.make(),
-                  ),
-                  CupertinoFormRow(
-                    child: CupertinoTextFormFieldRow(
-                      obscureText: true,
-                      placeholder: "Confirm Password",
+                    CupertinoFormRow(
+                      child: CupertinoTextFormFieldRow(
+                        obscureText: true,
+                        placeholder: "Confirm Password",
+                      ),
+                      prefix: "Confirm Password".text.make(),
                     ),
-                    prefix: "Confirm Password".text.make(),
-                  ),
-                ],
-              ),
-              CupertinoFormSection(
-                header: "Terms & Conditions".text.make(),
-                children: [
-                  CupertinoFormRow(
-                    child: CupertinoSwitch(
-                      value: true,
-                      onChanged: (value) {},
+                  ],
+                ),
+                CupertinoFormSection(
+                  header: "Terms & Conditions".text.make(),
+                  children: [
+                    CupertinoFormRow(
+                      child: CupertinoSwitch(
+                        value: true,
+                        onChanged: (value) {},
+                      ),
+                      prefix: "I Agree".text.make(),
                     ),
-                    prefix: "I Agree".text.make(),
-                  ),
-                ],
-              ),
-              20.heightBox,
-              Material(
-                color: context.theme.buttonColor,
-                borderRadius: BorderRadius.circular(8),
-                child: InkWell(
-                  onTap: () {
-                    Navigator.pushNamed(context, MyRoutes.loginRoute);
-                  },
-                  child: AnimatedContainer(
-                    duration: Duration(seconds: 1),
-                    width: 150,
-                    height: 50,
-                    alignment: Alignment.center,
-                    child: Text(
-                      "SignUp",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
+                  ],
+                ),
+                20.heightBox,
+                Material(
+                  color: context.theme.buttonColor,
+                  borderRadius: BorderRadius.circular(8),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, MyRoutes.loginRoute);
+                    },
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: 150,
+                      height: 50,
+                      alignment: Alignment.center,
+                      child: Text(
+                        "SignUp",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ).centered(),
-            ],
+                ).centered(),
+              ],
+            ),
           ),
         ),
       ),
